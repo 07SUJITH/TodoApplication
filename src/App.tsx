2e8de0a49f8b800f061fc69e34c1819d8249e295
SUJITH T S <tssujith2002@gmail.com>
2024-05-05 11:55:40 +0530

    chore: Update App component to use react-router for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router ,Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Signup from "./pages/signup/Signup"
+import Login from "./pages/login/Login"
+import TodoDashboard from "./components/TodoDashboard/TodoDashboard"
 
 function App() {
   return (
-    <>
-      <h1>Todo Application</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}> </Route>
+        <Route path="/signup" element={<Signup/>}> </Route>
+        <Route path="/login" element={<Login/>}> </Route>
+        <Route path="/todo" element={<TodoDashboard/>}> </Route>
+      </Routes>
+    </Router>
   )
 }
 
